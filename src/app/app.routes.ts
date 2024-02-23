6a4b14ab335c54ee1fce61198c1b3edcb7daa801
Silva Cunha Lucas Caio (Contractor) <lucas.i.silva@capgemini.com>
2024-02-23 12:36:40 -0300

    commit
    commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { InterComponent } from './inter/inter.component';
-
-export const routes: Routes = [
-    {path: 'inter', component: InterComponent}
-];
